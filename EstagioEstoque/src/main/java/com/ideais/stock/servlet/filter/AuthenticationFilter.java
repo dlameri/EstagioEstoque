2a42d852b888ea9e981bc494cabbb9bf47f2d118
JoseJunior <josejunior@jotajota-PC.(none)>
2014-03-14 16:22:37 -0300

    Alterações no WEB-Content.

--- EstagioEstoque/src/main/java/com/ideais/stock/servlet/filter/AuthenticationFilter.java
+++ EstagioEstoque/src/main/java/com/ideais/stock/servlet/filter/AuthenticationFilter.java
@@ -34,9 +34,9 @@ public class AuthenticationFilter implements Filter {
          
         HttpSession session = req.getSession(false);
          
-        if(session == null && !(uri.endsWith("html") || uri.endsWith("LoginServlet"))){
+        if(session == null && !(uri.endsWith("jsp") || uri.endsWith("LoginServlet"))){
             this.context.log("Unauthorized access request");
-            res.sendRedirect("/EstagioEstoque/index.html");
+            res.sendRedirect("/EstagioEstoque/index.jsp");
         }else{
             chain.doFilter(request, response);
         }    
